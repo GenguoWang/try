94134aeedc66dec020252c0c9b7913e88d75cd4f
Kingo Wang <wanggenguo@outlook.com>
2014-05-15 04:34:23 +0800

    Aysnc Http Client

--- tryAsio/HttpClient.h
+++ tryAsio/HttpClient.h
@@ -1,3 +1,6 @@
+#ifndef HTTPCLIENT_H_
+#define HTTPCLIENT_H_
+
 #include <string>
 #include "HttpUtility.h"
 #include <boost/array.hpp>
@@ -5,7 +8,172 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <istream>
+#include <ostream>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 using boost::asio::ip::tcp;
+
+class AsyncHttpRequest
+{
+public:
+  AsyncHttpRequest(boost::asio::io_service& io_service,
+      const std::string& server, const std::string &port,const std::string message,boost::function<void(const std::string &)> f)
+    : resolver_(io_service),
+      socket_(io_service),
+      request_(message),
+      callback(f)
+  {
+    // Start an asynchronous resolve to translate the server and service names
+    // into a list of endpoints.
+    tcp::resolver::query query(server, port);
+    resolver_.async_resolve(query,
+        boost::bind(&AsyncHttpRequest::handle_resolve, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::iterator));
+  }
+
+private:
+  void handle_resolve(const boost::system::error_code& err,
+      tcp::resolver::iterator endpoint_iterator)
+  {
+    if (!err)
+    {
+      // Attempt a connection to each endpoint in the list until we
+      // successfully establish a connection.
+      boost::asio::async_connect(socket_, endpoint_iterator,
+          boost::bind(&AsyncHttpRequest::handle_connect, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  void handle_connect(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // The connection was successful. Send the request.
+      boost::asio::async_write(socket_, boost::asio::buffer(request_),
+          boost::bind(&AsyncHttpRequest::handle_write_request, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  void handle_write_request(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Read the response status line. The response_ streambuf will
+      // automatically grow to accommodate the entire line. The growth may be
+      // limited by passing a maximum size to the streambuf constructor.
+      boost::asio::async_read_until(socket_, response_, "\r\n",
+          boost::bind(&AsyncHttpRequest::handle_read_status_line, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  void handle_read_status_line(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Check that response is OK.
+      std::istream response_stream(&response_);
+      std::string http_version;
+      response_stream >> http_version;
+      unsigned int status_code;
+      response_stream >> status_code;
+      std::string status_message;
+      std::getline(response_stream, status_message);
+      if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+      {
+        throw std::runtime_error("Invalid response"); 
+      }
+      if (status_code != 200)
+      {
+        throw std::runtime_error("Response returned with status code"+ status_code);
+      }
+
+      // Read the response headers, which are terminated by a blank line.
+      boost::asio::async_read_until(socket_, response_, "\r\n\r\n",
+          boost::bind(&AsyncHttpRequest::handle_read_headers, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  void handle_read_headers(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Process the response headers.
+      /*
+      std::istream response_stream(&response_);
+      std::string header;
+      while (std::getline(response_stream, header) && header != "\r")
+        std::cout << header << "\n";
+      std::cout << "\n";
+
+      // Write whatever content we already have to output.
+      if (response_.size() > 0)
+        std::cout << &response_;
+      */
+
+      // Start reading remaining data until EOF.
+      boost::asio::async_read(socket_, response_,
+          boost::asio::transfer_at_least(1),
+          boost::bind(&AsyncHttpRequest::handle_read_content, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  void handle_read_content(const boost::system::error_code& err)
+  {
+    std::ostringstream ostr;
+    if (!err)
+    {
+      // Write all of the data that has been read so far.
+      ostr << &response_;
+
+      // Continue reading remaining data until EOF.
+      boost::asio::async_read(socket_, response_,
+          boost::asio::transfer_at_least(1),
+          boost::bind(&AsyncHttpRequest::handle_read_content, this,
+            boost::asio::placeholders::error));
+    }
+    else if(err == boost::asio::error::eof)
+    {
+        callback(ostr.str());
+    }
+    else
+    {
+      throw boost::system::system_error(err); 
+    }
+  }
+
+  tcp::resolver resolver_;
+  tcp::socket socket_;
+  std::string request_;
+  boost::asio::streambuf response_;
+  boost::function<void(const std::string &)> callback;
+};
 
 class HttpClient
 {
@@ -13,6 +181,8 @@ private:
     typedef std::map<std::string,std::string> HD;
     typedef HD::iterator HD_IT;
     HD head;
+    std::vector<AsyncHttpRequest*> asyncRequests;
+    boost::asio::io_service io_service;
     void initHead()
     {
         head["Connection"]= "keep-alive";
@@ -28,6 +198,14 @@ private:
         }
         return str;
     }
+    void release_requests()
+    {
+        for(int i=0;i<asyncRequests.size();++i)
+        {
+            delete asyncRequests[i];
+        }
+        asyncRequests.clear();
+    }
 public:
     std::string resHeadStr;
     std::string resBodyStr;
@@ -40,6 +218,10 @@ public:
     HttpClient()
     {
         initHead();
+    }
+    ~HttpClient()
+    {
+        release_requests();
     }
     static void parseUrl(std::string url,ReqAddr & addr)
     {
@@ -107,9 +289,32 @@ public:
         head["Content-Length"] = oStream.str();
         return send(addr.host,addr.port,"POST "+addr.path+" HTTP/1.1\r\n"+headStr()+"\r\n"+formStr);
     }
+    void asyncGet(std::string url,boost::function<void(const std::string &)> callback)
+    {
+        ReqAddr addr;
+        parseUrl(url,addr);
+        AsyncHttpRequest *request = new AsyncHttpRequest(io_service, addr.host, addr.port,"GET "+addr.path+" HTTP/1.1\r\n"+headStr()+"\r\n",callback);
+        asyncRequests.push_back(request);
+    }
+    void asyncPost(std::string url,std::map<std::string,std::string> kv, boost::function<void(const std::string &)> callback)
+    {
+        ReqAddr addr;
+        parseUrl(url,addr);
+        head["Host"] = addr.host+":"+addr.port;
+        std::string formStr = getFormStr(kv);
+        std::ostringstream oStream;
+        oStream << formStr.size();
+        head["Content-Length"] = oStream.str();
+        AsyncHttpRequest *request = new AsyncHttpRequest(io_service, addr.host, addr.port,"POST "+addr.path+" HTTP/1.1\r\n"+headStr()+"\r\n"+formStr,callback);
+        asyncRequests.push_back(request);
+    }
+    void run()
+    {
+        io_service.run();
+        //release_requests();
+    }
     std::string send(std::string host,std::string port,std::string message)
     {
-        boost::asio::io_service io_service;
         tcp::resolver resolver(io_service);
         tcp::resolver::query query(host, port);
         tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
@@ -140,3 +345,5 @@ public:
         return resBodyStr;
     }
 };
+
+#endif
--- tryAsio/httpClient.cpp
+++ tryAsio/httpClient.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <map>
 using namespace std;
+void callback(const string & content)
+{
+    cout << "result:" << endl;
+    cout << content << endl;
+}
 int main(int argc,char *argv[])
 {
     HttpClient hc;
     map<string,string> kv;
     kv[argv[2]] = argv[3];
     cout << "hhh" << endl;
-    cout << hc.post(argv[1],kv) <<endl;
+    hc.asyncPost(argv[1],kv,callback);
+    hc.run();
     return 0;
 }
